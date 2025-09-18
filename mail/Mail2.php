636c4e38a7cd7223c72b0972b48d8e8ad6549e7e
Anantha700 <rockyanantha7@gmail.com>
2025-09-18 09:20:30 +0530

    Add files via upload

--- mail/Mail2.php
+++ mail/Mail2.php
@@ -1,52 +1,14 @@
-<?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require "PHPMailerAutoload.php";
-
-if ($_SERVER['REQUEST_METHOD']=="POST") {
-    $name = isset($_POST['name']) ? trim(htmlspecialchars($_POST['name'])) : '';
-    $email = isset($_POST['email']) ? trim(htmlspecialchars($_POST['email'])) : '';
-    $message = isset($_POST['message']) ? trim(htmlspecialchars($_POST['message'])) : '';
-
-
-try {
-    //Server settings
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';   // Your SMTP server
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'rockyanantha7@gmail.com'; // Your email
-    $mail->Password   = 'vdkw smfz aics qwsb';    // App password or SMTP password
-    $mail->SMTPSecure = 'tls';                  // 'ssl' also works if supported
-    $mail->Port       = 587;
-
-    //Recipients
-    $mail->setFrom('rockyanantha7@gmail.com', 'Anantha');
-    $mail->addAddress('rockyanantha7.97@gmail.com');
-
-    //Content
-    $mail->isHTML(true);
-    $mail->Subject = 'Test Email';
-    $mail->Body    = '<h1>Welcome to </h1>';
-    $mail->AltBody = 'Welcome to SJC';
-
-    $mail->send();
-    echo 'Message has been sent successfully';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-}
-?>
 
 <!DOCTYPE html>
-<html lan="en">
+<html lang="en">
 <head>
   <title>Send Mail</title>
 </head>
 <body>
-  <form action="Mail2.php" method ="post">
-    <input type="text" placeholder="Enter Name" name= "name"><br>
-    <input type="email" placeholder="Enter Email" name= "email"><br>
-    <input type="submit" value="submit" name="submit">
+  <form action="Index.php" method="post">
+    <input type="text" name="name" placeholder="Enter Name"><br>
+    <input type="email" name="email" placeholder="Enter Email"><br>
+    <input type="submit" name="submit" value="Submit">
   </form>
 </body>
 </html>
